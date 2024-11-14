00d651f6c093ada7dd5c82e53c792c76220e4628
Rohollah <Rohollahhosseini00@gmail.com>
2024-11-14 18:51:12 +0330

    Added MVC Project and Use Rate Limiting

---
+++ src/RateLimiting/RateLimiting in MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using RateLimiting_in_MVC.Models;
+using System.Diagnostics;
+
+namespace RateLimiting_in_MVC.Controllers
+{
+    //استفاده کرد Atribute باید از این RateLimiting برای استفاده از 
+    [EnableRateLimiting("MyRateFixed")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [EnableRateLimiting("MyRateFixed")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        //استفاده کنیم Attribute استفاده نکینم باید از این RateLimit اگر بخواهیم از
+        [DisableRateLimiting]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/RateLimiting/RateLimiting in MVC/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region Rate Limiting
+
+//1-Fixed Window Limit
+builder.Services.AddRateLimiter(p =>
+{
+    p.AddFixedWindowLimiter("MyRateFixed", options =>
+    {
+        options.PermitLimit = 3;//تعداد در خواست 
+        options.Window = TimeSpan.FromSeconds(10);//زمان اجرای درخواست ها
+       // options.QueueLimit = 1;//اگر درخواست ها بیش از حد بود اینجا تعدادی را در صف قرار بده
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;//خواندن درخواست ها که به صورت پیش فرض از قدیمی ترین درخواست خوانده میشود
+    });
+
+});
+
+#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//قرار بگیرد Routingبعد از میانافزار مربوط به  RateLimiting باید میان افزار مربوط به 
+app.UseRateLimiter();
+
+app.Run();
